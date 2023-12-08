21b2d9ab5e09b3973df931fe81feaee2aef0db51
Hritu Toma <hritu.tomavlad@gmail.com>
2023-12-08 18:52:24 +0200

    Added mmm file

--- mmm.js
+++ mmm.js
@@ -1,89 +1,76 @@
-const matrixA = [
-    [1, 2, 3, 0, 0],
-    [0, 4, 5, 6, 0],
-    [0, 0, 7, 8, 9]
+const a = [
+  [1, 2, 3, 0, 0],
+  [0, 4, 5, 6, 0],
+  [0, 0, 7, 8, 9]
 ];
 
-const matrixB = [
-    [1, 0, 0],
-    [2, 4, 0],
-    [3, 5, 7],
-    [0, 6, 8],
-    [0, 0, 9]
+const b = [
+  [1, 0, 0],
+  [2, 4, 0],
+  [3, 5, 7],
+  [0, 6, 8],
+  [0, 0, 9]
 ];
 
-class Cell {
-    constructor() {
-        this.c = 0;
-        this.ain = 0;
-        this.aout = 0;
-        this.uin = 0;
-        this.uout = 0;
-    }
-}
+const cells = Array.from({ length: a.length }, () =>
+  Array.from({ length: b[0].length }, () => ({ c: 0, ain: 0, bin: 0, aout: 0, bout: 0 }))
+);
 
-const numRowsA = matrixA.length;
-const numColsA = matrixA[0].length;
-const numRowsB = matrixB.length;
-const numColsB = matrixB[0].length;
-
-const cells = Array.from({ length: numRowsA }, () => Array(numColsB).fill(new Cell()));
-
-function printStates(stepIndex) {
-    console.log(`\nStep ${stepIndex + 1}:\n`);
-    cells.forEach((row) => {
-        row.forEach((cell) => {
-            console.log(`c: ${cell.c}, ain: ${cell.ain}, aout: ${cell.aout}, uin: ${cell.uin}, uout: ${cell.uout}`);
-        });
-    });
-}
+let inputIndex = 0;
 
 function step() {
-    for (let i = 0; i < numRowsA; ++i) {
-        for (let j = 0; j < numColsB; ++j) {
-            if (i < numRowsA && j < numColsA) {
-                cells[i][j].ain = matrixA[i][j];
-            } else {
-                cells[i][j].ain = 0;
-            }
-
-            if (i > 0) {
-                cells[i][j].uin = cells[i - 1][j].uout;
-            } else {
-                cells[i][j].uin = (j < numColsB) ? matrixB[0][j] : 0;
-            }
-        }
+  // Propagate input from matrix a
+  for (let i = 0; i < cells.length; ++i) {
+    for (let j = 0; j < cells[0].length; ++j) {
+      if (j === 0) {
+        cells[i][j].ain = inputIndex < a[i].length ? a[i][inputIndex] : 0;
+      } else {
+        cells[i][j].ain = cells[i][j - 1].aout;
+      }
     }
+  }
 
-    for (let i = 0; i < numRowsA; ++i) {
-        for (let j = 0; j < numColsB; ++j) {
-            cells[i][j].c += cells[i][j].ain * cells[i][j].uin;
-            cells[i][j].aout = cells[i][j].ain;
-            cells[i][j].uout = cells[i][j].uin;
-        }
+  // Propagate input from matrix b
+  for (let i = 0; i < cells.length; ++i) {
+    for (let j = 0; j < cells[0].length; ++j) {
+      if (i === 0) {
+        cells[i][j].bin = inputIndex < b.length ? b[inputIndex][j] : 0;
+      } else {
+        cells[i][j].bin = cells[i - 1][j].bout;
+      }
     }
+  }
+
+  for (let i = 0; i < cells.length; ++i) {
+    for (let j = 0; j < cells[0].length; ++j) {
+      cells[i][j].c += cells[i][j].ain * cells[i][j].bin;
+      cells[i][j].aout = cells[i][j].ain;
+      cells[i][j].bout = cells[i][j].bin;
+    }
+  }
+
+  ++inputIndex;
 }
 
 console.log("Matrix A is");
-matrixA.forEach((row) => {
-    console.log(row.join(' '));
-});
+a.forEach(row => console.log(row.join(" ")));
 
 console.log("\nMatrix B is");
-matrixB.forEach((row) => {
-    console.log(row.join(' '));
-});
+b.forEach(row => console.log(row.join(" ")));
 
-console.log("\nInitial states:\n");
-printStates(-1);
+for (let stepIndex = 0; stepIndex < a.length + b.length - 1; ++stepIndex) {
+  console.log(`\nStep ${stepIndex + 1}:`);
+  step();
 
-for (let stepIndex = 0; stepIndex < numColsA; ++stepIndex) {
-    step();
-    printStates(stepIndex);
+  for (let i = 0; i < cells.length; ++i) {
+    for (let j = 0; j < cells[0].length; ++j) {
+      console.log(`c: ${cells[i][j].c}, ain: ${cells[i][j].ain}, bin: ${cells[i][j].bin}`);
+    }
+    console.log("");
+  }
 }
 
-// Print the resulting matrix
-const resultMatrix = cells.map(row => row.map(cell => cell.c));
-console.log("\nResulting Matrix:");
-console.log(resultMatrix);
 
+// Display the final matrix
+console.log("\nFinal Matrix is");
+cells.forEach(row => console.log(row.map(cell => cell.c).join(" ")));
